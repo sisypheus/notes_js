7bd018dbf41ffaf0274441a03754f4cc2511fe37
tpoette <theo.poette@epitech.eu>
2020-08-12 23:42:20 +0200

    [+] displaying form for new todo but problems to get back form values

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,11 +1,24 @@
+//on add task hide button and display form
+
 let button = document.getElementById('ajouter');
+let ajout = document.getElementById('ajout');
+
+ajout.style.display = 'none';
 
 button.addEventListener('click', () => {
-    let todos = document.getElementById('todos');
+    let ajout = document.getElementById('ajout');
+
+    if (ajout.style.display === 'none')
+        ajout.style.display = 'flex';
+    else
+        ajout.style.display = 'none';
+    /*let todos = document.getElementById('todos');
     let text = 'hello';
 
     let node = document.createElement('p');
     node.appendChild(document.createTextNode(text));
-    todos.appendChild(node);
-    //console.log('ajouter');
+    todos.appendChild(node);*/
 });
+
+let title = document.getElementById('todo_title').value;
+console.log(title);
