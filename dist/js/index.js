809d03272e2b2fc02afb8e496a95de24e4bc82a1
tpoette <theo.poette@epitech.eu>
2020-08-15 00:06:35 +0200

    [+] removing notes, code is ugly af and needs to have an array display system

--- dist/js/index.js
+++ dist/js/index.js
@@ -4,24 +4,40 @@ let submit = document.getElementById('submit');
 
 ajout.style.display = 'none';
 
-function add_todo(title, content) {
+function add_note(title, content) {
     //variables
     let notes = document.getElementById('notes');
     let div_container = document.createElement('div');
     let node = document.createElement('p');
     let node_title = document.createElement('h2');
-    if (typeof add_todo.counter == 'undefined')
-        add_todo.counter = 1;
+    let remove = document.createElement('button');
 
+    if (typeof add_note.counter == 'undefined')
+        add_note.counter = 1;
+    
+    remove.innerHTML = 'Remove';
+    remove.id = add_note.counter;
+    remove.addEventListener('click', () => {
+        let to_delete = document.querySelector('#note'+remove.id);
+
+        to_delete.remove();
+        //console.log(to_delete);
+    });
     div_container.className = "note_container";
-    div_container.id = add_todo.counter;
+    div_container.id = 'note' + add_note.counter;
+
     //user input sanitized with create text node function
     node_title.appendChild(document.createTextNode(title));
+    node_title.appendChild(remove);
     node.appendChild(document.createTextNode(content));
     node.prepend(node_title);
     div_container.appendChild(node);
     notes.appendChild(div_container);
-    add_todo.counter++;
+    add_note.counter++;
+}
+
+function remove_note(id) {
+    console.log(id);
 }
 
 button.addEventListener('click', () => {
@@ -48,7 +64,7 @@ submit.addEventListener('click', () => {
     let content = document.getElementById('note_value').value;
 
     if (title != "" && content != "")
-        add_todo(title, content);
+        add_note(title, content);
 
     document.getElementById('note_title').value = "";
     document.getElementById('note_value').value = "";
