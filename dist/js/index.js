9dfb08c95e07fdb695f5221b87667ad1ff17ce4a
tpoette <theo.poette@epitech.eu>
2020-08-14 20:18:44 +0200

    [+] div for notes

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,5 +1,3 @@
-//on add task hide button and display form
-
 let button = document.getElementById('ajouter');
 let ajout = document.getElementById('ajout');
 let submit = document.getElementById('submit');
@@ -7,31 +5,43 @@ let submit = document.getElementById('submit');
 ajout.style.display = 'none';
 
 function add_todo(title, content) {
+    //variables
     let notes = document.getElementById('notes');
-
+    let div_container = document.createElement('div');
     let node = document.createElement('p');
     let node_title = document.createElement('h2');
+    if (typeof add_todo.counter == 'undefined')
+        add_todo.counter = 1;
+
+    div_container.className = "note_container";
+    div_container.id = add_todo.counter;
     //user input sanitized with create text node function
     node_title.appendChild(document.createTextNode(title));
     node.appendChild(document.createTextNode(content));
     node.prepend(node_title);
-    notes.appendChild(node);
+    div_container.appendChild(node);
+    notes.appendChild(div_container);
+    add_todo.counter++;
 }
 
 button.addEventListener('click', () => {
     let ajout = document.getElementById('ajout');
+    let title = document.getElementById('note_title').value;
+    let content = document.getElementById('note_value').value;
 
     if (ajout.style.display === 'none')
         ajout.style.display = 'flex';
-    else
+    else {
         ajout.style.display = 'none';
+        document.getElementById('note_title').value = "";
+        document.getElementById('note_value').value = "";
+    }
 });
 
 let form = document.getElementById("new_note");
-function handleForm(event) {
+form.addEventListener('submit', (event) => {
     event.preventDefault();
-} 
-form.addEventListener('submit', handleForm);
+});
 
 submit.addEventListener('click', () => {
     let title = document.getElementById('note_title').value;
